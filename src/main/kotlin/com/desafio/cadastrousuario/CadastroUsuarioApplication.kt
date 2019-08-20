9a659c207db4839f32fd0ff8b5e540b29b8b213c
vitor.de.o.batista <vitor.de.o.batista@accenture.com>
2019-08-20 16:11:32 -0300

    inicializacao de projeto

---
+++ src/main/kotlin/com/desafio/cadastrousuario/CadastroUsuarioApplication.kt
@@ -0,0 +1,11 @@
+package com.desafio.cadastrousuario
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CadastroUsuarioApplication
+
+fun main(args: Array<String>) {
+	runApplication<CadastroUsuarioApplication>(*args)
+}
