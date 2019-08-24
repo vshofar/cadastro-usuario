0853c749772281cd8db94645b61e3f159b6f9d09
vitor batista <vitor.shofar@gmail.com>
2019-08-24 20:09:49 -0300

    reame do controller. adicao de arquivo de dados para povoamento

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into user
+values(1,'user@mail.com','user1', '123');
+
+insert into phone
+values(1,'00','9123-4567', 1);
+
+insert into phone
+values(2,'01','9123-4567', 1);
+
+select * from user
+
